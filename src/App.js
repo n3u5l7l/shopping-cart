179586279e10369e9415717328dbb26fa7d516a4
MingMing <greeteaz567@gmail.com>
2022-06-05 20:13:56 -0500

    Fixed Link style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import "./App.css";
 import {ReactComponent as LeftArrowLogo} from "./assets/chevron-left.svg";
 import {ReactComponent as RightArrowLogo} from "./assets/chevron-right.svg";
+import { Link } from "react-router-dom";
+import styled from "styled-components"
+import { motion } from "framer-motion";
 
+const StyledLink = styled(Link)`
+  border:none;
+  padding:1rem;
+  color:black;
+  background-color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  opacity:0.8;
+  text-decoration: none;
+  transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+
+  &:hover {
+    opacity:1;
+    transform: scale(1.1);
+  }
+`;
 
 function App() {
   return (
-    <div className="main">
+    <motion.div initial={{x:300, opacity:0}} 
+    animate={{x:0, opacity:1}} 
+    exit={{x: window.innerWidth, opacity:1, transition:{duration: 0.3}}} className="main">
       <div className="welcome-text">Top selling nut shop of the year!</div>
       <div className="nutrition-text">
         Our nuts provide you with vast amounts of vitamins/minears 
@@ -13,10 +34,10 @@ function App() {
       </div>
       <div className="shop-button-container">
         <RightArrowLogo className="svgRight"/>
-        <button className="shop-button">VISIT THE SHOP NOW</button>
+        <StyledLink to="/shop" className="shop-button">VISIT THE SHOP NOW</StyledLink>
         <LeftArrowLogo className="svgLeft"/>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
