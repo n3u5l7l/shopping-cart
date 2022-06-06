da0f2fb81e959685d2b63630bc4cd1cc5b7f716d
MingMing <greeteaz567@gmail.com>
2022-06-05 20:17:16 -0500

    Fixed Link styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,23 @@
 import { ReactComponent as CartLogo } from "../assets/cart-outline.svg";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+    position: relative;
+    text-decoration: none;
+    color:white;
+&::after{
+    content: "";
+    display:block;
+    border-bottom: 1px solid;
+    transform: scaleX(0);
+    transition: transform 0.2s ease-in-out;
+    }
+&:hover::after{
+        transform: scaleX(1);
+    }
+`
 
 function Header () {
     const content = (
@@ -9,10 +26,10 @@ function Header () {
             <div className="title">NutShopy</div>
             <div className="links-container">
                 <nav className="links">
-                    <Link to="/">Home</Link>
-                    <Link to ="/shop">Shop</Link>
-                    <Link to="/contact">Contact</Link>
-                    <Link to="/cart"><CartLogo/></Link>
+                    <StyledLink to="/">Home</StyledLink>
+                    <StyledLink to ="/shop">Shop</StyledLink>
+                    <StyledLink to="/contact">Contact</StyledLink>
+                    <StyledLink to="/cart"><CartLogo/></StyledLink>
                 </nav>
             </div>
         </header>
