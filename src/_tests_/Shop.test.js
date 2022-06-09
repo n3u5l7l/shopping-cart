e91456cc25d08b9697faf92a1e89205a39d5981d
MingMing <greeteaz567@gmail.com>
2022-06-09 18:43:03 -0500

    Added test for Shop

--- src/_tests_/Shop.test.js
+++ src/_tests_/Shop.test.js
@@ -1,6 +1,8 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Shop from "../components/Shop";
+import React from "react";
+import userEvent from "@testing-library/user-event";
 
 afterEach(cleanup);
 
@@ -9,3 +11,33 @@ it("renders properly", () => {
 
     expect(container).toMatchSnapshot();
 });
+
+it("renders all shop item button", async () => {
+    render(<Shop />);
+    const allButtons = screen.getAllByRole('button');
+
+    expect(allButtons).toHaveLength(allButtons.length);
+});
+
+it("renders all images", () => {
+    render(<Shop />);
+    
+    const allImages = screen.getAllByRole("img");
+
+    expect(allImages).toHaveLength(allImages.length);
+});
+
+test("all buttons can be clicked on shop items", async () => {
+    const updateCartMock = jest.fn();
+    render(<Shop updateCart = {updateCartMock}/>);
+    const allButtons = screen.getAllByRole('button');
+
+    for(let i = 0; i < allButtons.length; i++){
+        await userEvent.click(allButtons[i]);
+    }
+
+    expect(updateCartMock).toHaveBeenCalledTimes(allButtons.length);
+});
+
+
+
