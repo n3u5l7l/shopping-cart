1c27bb89c799152fb0e16ed3b5fc2e60c590e86b
MingMing <greeteaz567@gmail.com>
2022-06-08 19:01:21 -0500

    Updated routing

--- src/components/Router/AnimatePresence.js
+++ src/components/Router/AnimatePresence.js
@@ -1,16 +1,16 @@
 import { Routes, Route, useLocation } from "react-router-dom"
 import App from "../../App";
 import Shop from "../Shop";
+import Contact from "../Contact";
 import { AnimatePresence } from "framer-motion";
-function AnimatePresences () {
+function AnimatePresences ({handleShoppingCart: setShoppingCart}) {
     const location = useLocation();
     return(
         <AnimatePresence initial={false} exitBeforeEnter>
             <Routes location={location} key={location.pathname}>
                 <Route path="/" element={<App />} />
-                <Route path="/shop" element={<Shop/>} />
-                <Route path="/contact" element={null} />
-                <Route path="/cart" element={null} />
+                <Route path="/shop" element={<Shop handleShoppingCart = {setShoppingCart}/>} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </AnimatePresence>
     )
