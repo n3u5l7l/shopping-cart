48a06ce9734023ed1b6152948a24fa3bad1dc5fd
MingMing <greeteaz567@gmail.com>
2022-06-08 19:02:20 -0500

    Added State && Props + new Cart component

--- src/components/Router/RouterSwitcher.js
+++ src/components/Router/RouterSwitcher.js
@@ -1,13 +1,26 @@
 import { BrowserRouter, Routes, Route, useSearchParams } from "react-router-dom";
-import App from "../../App";
 import Navigation from "../Header";
+import Cart from "../Cart";
 import AnimatePresence from "./AnimatePresence";
+import { useEffect, useState } from "react";
 
 const RouterSwitcher = () => {
+    const [shoppingCart, setShoppingCart] = useState({totalCost: 0, totalItem: 0});
+    const [showCart, setShowCart] = useState(false);
+    
+    function changeCartStatus(){
+        setShowCart((showCart) => !showCart);
+    }
+
+    function dontCloseCart(e){
+        e.stopPropagation();
+    }
+
     return (
         <BrowserRouter>
-            <Navigation />
-            <AnimatePresence />
+            <Navigation shoppingCart={shoppingCart} changeCartStatus={changeCartStatus}/>
+            <AnimatePresence handleShoppingCart = {setShoppingCart}/>
+            <Cart shoppingCart={shoppingCart} setShoppingCart={setShoppingCart} showCart={showCart} changeCartStatus={changeCartStatus} dontCloseCart={dontCloseCart}/>
         </BrowserRouter>
     )
 }
