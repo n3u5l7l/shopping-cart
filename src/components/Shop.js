e6424c127459924e0543e3a0ec16433cf9e2b12a
MingMing <greeteaz567@gmail.com>
2022-06-08 19:06:48 -0500

    Added event handling && cart updater

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -10,32 +10,32 @@ import MacadamiaNutPic from "../assets/Macadamia-nut.jpeg";
 import PeanutPic from "../assets/peanut.jpeg";
 import BrazilNutPic from "../assets/Brazil-nut.jpeg";
 
-function Shop (){
+function Shop ({handleShoppingCart: setShoppingCart}){
     const NutInfos = [
         {
             image: CashewPic,
             type: "Cashew nuts",
-            cost: "$15.99"
+            cost: "15.99"
         },
         {
             image: AlmondPic,
             type: "Almonds",
-            cost: "$25.99"
+            cost: "25.99"
         },
         {
             image: WalnutPic,
             type: "Walnuts",
-            cost: "$14.99"
+            cost: "14.99"
         },
         {
             image: PecanPic,
             type: "Pecans",
-            cost: "$14.99"
+            cost: "14.99"
         },
         {
             image: PistachiosPic,
             type: "Pistachios",
-            cost: "$20.99"
+            cost: "20.99"
         },
         {
             image: HazlenutPic,
@@ -59,6 +59,7 @@ function Shop (){
         }
     ];
 
+    const updateCart = (nutType, nutCost, nutImg) => {setShoppingCart((prevState) => Object.assign({}, prevState, prevState[nutType] && prevState[nutType].amount >= 0  ? {[nutType]: {...prevState[nutType], amount: Number(prevState[nutType].amount)+1, total: Number(prevState[nutType].amount) * Number(prevState[nutType].cost)}, totalCost: prevState["totalCost"] + nutCost, totalItem: prevState["totalItem"] + 1}: {[nutType]: {amount: 1, cost: nutCost, image: nutImg}, totalCost: prevState.totalCost ? prevState.totalCost + nutCost: nutCost, totalItem: prevState.totalItem ? prevState.totalItem + 1: 1} )); console.log("LoL");};
     let content = (
         NutInfos.map((nut, index) => {
             return(
@@ -66,8 +67,8 @@ function Shop (){
                 <img src={nut.image} alt={nut.type + " pic"} />
                 <div className="item-info">
                     <div className="nut-type">{nut.type}</div>
-                    <div className="nut-cost">{nut.cost}</div>
-                    <button>Add to cart</button>
+                    <div className="nut-cost">${nut.cost}</div>
+                    <button onClick={() => updateCart(nut.type, Number(nut.cost), nut.image)}>Add to cart</button>
                 </div>
             </div>
             )
